10d26e9b0229572b5fb3ee5fe39d6cc10d8dbce9
dsv <zxcv>
2024-10-01 00:18:19 +0500

    Initial Commit

---
+++ Home Chore Scheduler/Home_Chore_SchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_Chore_SchedulerApp.swift
+//  Home Chore Scheduler
+//
+//  Created by Muhammad Rizwan on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Home_Chore_SchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
